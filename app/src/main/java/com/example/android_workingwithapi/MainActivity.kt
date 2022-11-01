89e540d9ab525654d90b3c67ca188d0983add48a
ZheniKoleva <zhenie_koleva@abv.bg>
2022-11-01 15:58:53 +0200

    Main Activity's onCreate method added

--- app/src/main/java/com/example/android_workingwithapi/MainActivity.kt
+++ app/src/main/java/com/example/android_workingwithapi/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.android_workingwithapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_workingwithapi.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://restcountries.com/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val countryService = retrofit.create(CountryService::class.java)
+        val countryRepository = CountryRepository(countryService)
+
+        countryRepository.getCountries()?.enqueue(object : Callback<List<Country>> {
+            override fun onResponse(call: Call<List<Country>>, response: Response<List<Country>>) {
+                val countries = response.body() ?: return
+                val adapter = CountryAdapter(countries)
+                binding.countriesList.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Country>>, t: Throwable) {
+                Snackbar.make(binding.root, "Failure to load countries", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        })
     }
 }
