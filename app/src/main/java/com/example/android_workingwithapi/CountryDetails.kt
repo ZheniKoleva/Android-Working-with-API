1fc41fbb72e5e7577b56409d49036c5c05679517
ZheniKoleva <zhenie_koleva@abv.bg>
2022-11-02 11:39:12 +0200

    new data class added

---
+++ app/src/main/java/com/example/android_workingwithapi/CountryDetails.kt
@@ -0,0 +1,10 @@
+package com.example.android_workingwithapi
+
+data class CountryDetails(
+    val name: String,
+    val capital: String,
+    val flags: Flag,
+    val region: String,
+    val population: Int,
+    val area: Double,
+)
