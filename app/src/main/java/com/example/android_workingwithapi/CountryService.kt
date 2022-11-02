3d4805a11298b8f2f042f62ac4eacbd3dcf9f3f0
ZheniKoleva <zhenie_koleva@abv.bg>
2022-11-02 12:16:25 +0200

    service correction

--- app/src/main/java/com/example/android_workingwithapi/CountryService.kt
+++ app/src/main/java/com/example/android_workingwithapi/CountryService.kt
@@ -2,11 +2,12 @@ package com.example.android_workingwithapi
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CountryService {
     @GET("all")
     fun getAllCountries(): Call<List<Country>>
 
-    @GET("name/{name}")
-    fun getCountryDetails(name: String) : Call<CountryDetails>
+    @GET("name/{countryName}")
+    fun getCountryDetails(@Path("countryName") name: String) : Call<CountryDetails>
 }
